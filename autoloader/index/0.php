ec3730891c6e4b044e970cca7eac23fff1928bc9
andreas4all <andreas4all@users.noreply.github.com>
2014-04-01 23:32:16 +0200

    Autoloader

---
+++ autoloader/index/0.php
@@ -0,0 +1,20 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Class index for autoloading
+ * 
+ * This class index was generated automatically.
+ * Don't edit this file. Changes might get lost when
+ * generating a new index.
+ *
+ * @see  Autoloader::buildIndex()
+ * @see  AutoloaderIndex_PHPArrayCode
+ * @link http://php-autoloader.malkusch.de/en/
+ */
+
+return array(
+    'cliphpy\lib\element' => '../lib/element.php',
+    'testelement' => '../lib/elementTest.php',
+);
