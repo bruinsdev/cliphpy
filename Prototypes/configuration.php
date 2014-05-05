d16b1e930f7c48b424536b152accb38ad19a395c
andreas4all <andreas4all@users.noreply.github.com>
2014-05-05 20:12:39 +0200

    Configuration - changed visibility for $environmentVariableName

--- Prototypes/configuration.php
+++ Prototypes/configuration.php
@@ -16,12 +16,12 @@ class Configuration
   /**
    * @var string
    */
-  private $environment = "production";
+  protected $environmentVariableName = "CLIPHPY_ENVIRONMENT";
 
   /**
    * @var string
    */
-  private $environmentVariableName = "CLIPHPY_ENVIRONMENT";
+  private $environment = "production";
 
   public function __construct(){
     ;
