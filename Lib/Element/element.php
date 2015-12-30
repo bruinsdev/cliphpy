66321244ef01617aa4744d79fd5107727247177d
andreas4all <andreas4all@users.noreply.github.com>
2015-12-30 10:36:44 +0100

    Lib\Element - added method getRandomString

--- Lib/Element/element.php
+++ Lib/Element/element.php
@@ -224,6 +224,23 @@ abstract class Element
   }
 
   /**
+   * @param  integer $length
+   * @return string
+   */
+  protected function getRandomString($length = 10){
+    $characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $randomString = "";
+    for ($i = 0; $i < $length; $i++) {
+      $randomString = sprintf(
+        "%s%s",
+        $randomString,
+        $characters[rand(0, strlen($characters) - 1)]
+      );
+    }
+    return $randomString;
+  }
+
+  /**
    * @return string
    */
   private function getDelayKey(){
